f9e211116fd1254bd6851a6535b868f0e8da3c10
kcchan258 <>
2025-09-25 13:56:39 +0800

    BE: Swagger UI added

--- backend/InteractiveHub.WebAPI/Program.cs
+++ backend/InteractiveHub.WebAPI/Program.cs
@@ -1,3 +1,10 @@
+using InteractiveHub;
+using InteractiveHub.Service.Logger;
+using Microsoft.OpenApi.Models;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +12,52 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
+        options.Audience = builder.Configuration["Auth0:Audience"];
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = $"https://{builder.Configuration["Auth0:Domain"]}/",
+            ValidAudience = builder.Configuration["Auth0:Audience"]
+        };
+    });
+
+#region Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "InteractiveHub.WebAPI", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Input : Bearer {你的JWT}",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+#endregion
+
+builder.Services.AddInteractiveHubServices(); // Add the HubLogger service
 
 var app = builder.Build();
 
@@ -12,6 +65,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "InteractiveHub API v1");
+        c.DisplayRequestDuration();
+    });
 }
 
 app.UseHttpsRedirection();
@@ -19,5 +78,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseInteractiveHubServices(); // Use the HubLogger middleware
 
 app.Run();
